63dca2be0595fc06d459059b88c81945d82fbf5a
wandl-6A72h <hiwepy@gmail.com>
2026-05-16 14:13:11 +0800

    refactor: migrate groupId and packages from com.github.hiwepy to io.github.hiwepy

---
+++ src/main/java/io/github/hiwepy/validation/constraints/IdCard.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.hiwepy.validation.constraints;
+
+import io.github.hiwepy.validation.internal.constraintvalidators.IdCardValidator;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * @author hiwepy
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = IdCardValidator.class)
+public @interface IdCard {
+	
+	/**
+	 * @return the error message template
+	 */
+	String message() default "{javax.validation.constraints.Pattern.message}";
+	
+}
