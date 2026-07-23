c74ca1e78213eec9d2614b3a24dcbf1d327b7b1d
wandl-6A72h <hiwepy@gmail.com>
2026-07-23 09:38:07 +0800

    chore: migrate Maven coordinates and packages to easy4j
    Update groupId to io.github.easy4j and relocate io/com.github.hiwepy
    packages to io.github.easy4j. Class names are unchanged.

---
+++ src/main/java/io/github/easy4j/validation/internal/constraintvalidators/IdCardValidator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.easy4j.validation.internal.constraintvalidators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import io.github.easy4j.validation.constraints.IdCard;
+import io.github.easy4j.validation.utils.IDCardUtils;
+import io.github.easy4j.validation.utils.IdcardUtils2;
+
+public class IdCardValidator implements ConstraintValidator<IdCard, String> {
+
+	@Override
+	public void initialize(IdCard annotation) {
+	}
+
+	@Override
+	public boolean isValid(String input, ConstraintValidatorContext context) {
+		return IDCardUtils.isIDCard(input) || IdcardUtils2.validateCard(input) || IdcardUtils2.validateHKCard(input)
+				|| IdcardUtils2.validateTWCard(input);
+	}
+
+}
